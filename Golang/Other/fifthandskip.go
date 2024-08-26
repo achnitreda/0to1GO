8666ea2e534b68e98156ff896a91fb093be79d5b
achnitreda <achnitreda@gmail.com>
2024-08-26 22:18:14 +0100

    fifthandSkip

--- Golang/Other/fifthandskip.go
+++ Golang/Other/fifthandskip.go
@@ -23,3 +23,43 @@ func FifthAndSkip(str string) string {
 	}
 	return string(res) + "\n"
 }
+
+
+
+// package piscine
+
+// func FifthAndSkip(str string) string {
+// 	if str == "" {
+// 		return "\n"
+// 	}
+// 	if len(str) < 5 {
+// 		return "Invalid Input\n"
+// 	}
+// 	runes := []rune(str)
+// 	i := 0
+// 	e := 0
+// 	res := ""
+// 	for e < len(runes) {
+// 		if runes[e] != ' ' {
+// 			i++
+// 			res += string(runes[e])
+// 		}
+// 		if e == len(runes)-1{
+// 			break
+// 		}
+// 		if e+1 < len(runes) && runes[e+1] == ' ' && i == 5 {
+// 			res += " "
+// 			i = 0
+// 			e += 2
+// 		} else {
+// 			if i == 5 {
+// 				res += " "
+// 				i = 0
+// 				e++
+// 			}
+// 		}
+// 		e++
+// 	}
+// 	res += "\n"
+// 	return res
+// }
